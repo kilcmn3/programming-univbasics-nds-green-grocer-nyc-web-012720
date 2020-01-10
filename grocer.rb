2bd49f4e95bc73b5735b8152107347d810e5d72c
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-10 21:34:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -112,5 +112,6 @@ def checkout(cart, coupons)
     i += 1
     grand_total += total.round(2)
   end
-return grand_total > 100 ? grand_total * 0.2 : grand_total
+  
+return grand_total > 100 ? discount = grand_total * 0.1 : grand_total
 end
